c46028443d6ad87af2570a13772a3fd11dafb6fe
vlson <lxj370832@163.com>
2019-01-17 14:09:03 +0800

    update favicon.ico

--- backend/web/js/login.js
+++ backend/web/js/login.js
@@ -6,7 +6,7 @@
 * 定时切换背景图
 * */
 function changeBackground(){
-    var img_num = Math.ceil(Math.random()*4);
+    var img_num = Math.ceil(Math.random()*6);
     $('body').css('background', 'url(/images/login/bg-image' + img_num + '.jpg) top center no-repeat');
     $('body').css('background-size', 'cover');
 }
