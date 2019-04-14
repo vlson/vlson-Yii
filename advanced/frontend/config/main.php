03931b84230820104c06a34ff4e4af5192426836
vlson <lxj370832@163.com>
2019-04-14 22:42:05 +0800

    frontend url && debug

--- advanced/frontend/config/main.php
+++ advanced/frontend/config/main.php
@@ -10,6 +10,7 @@ return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'zh-CN', // 语言包
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'request' => [
@@ -36,14 +37,20 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            //'suffix' => '.html',//后缀，如果设置了此项，那么浏览器地址栏就必须带上.html后缀，否则会报404错误
             'rules' => [
+                //http://frontend.com/site/index 重写为  http://frontend.com/site
+                '<controller:\w+>/' => '<controller>/index',
+                //http://frontend.com/site/view?id=1 重写为 http://frontend.com/site/1
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                //http://frontend.com/site/ceshi?id=123 重写为  http://frontend.com/site/ceshi/123
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
---
+++ advanced/frontend/controllers/PublicController.php
@@ -0,0 +1,8 @@
+<?php
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class PublicController extends Controller{
+
+}
